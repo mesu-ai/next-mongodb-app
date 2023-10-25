bd8ffcfaa872358d55cb1c6d43059af2721275ec
Md Ibrahim Adham Mesu <72233105+mesu-ai@users.noreply.github.com>
2023-10-25 11:40:43 +0000

    Update mongodb

--- src/app/topics/add/page.jsx
+++ src/app/topics/add/page.jsx
@@ -1,10 +1,38 @@
+'use client'
+
 import React from 'react';
 
 const TopicAddPage = () => {
+
+  const handleAddTopics = async () => {
+    try {
+      const res = await fetch('https://localhost:3000/api/topics', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: 'test', description: 'test' }),
+      });
+  
+      if (res.ok) {
+        const data = await res.json();
+        console.log('Success:', data);
+      } else {
+        console.log('Error:', res.status);
+      }
+    } catch (error) {
+      console.log('Network error:', error);
+    }
+  };
+  
+
   return (
     <div>
       <h1>Topic Add Page</h1>
-      
+
+      <button 
+      onClick={() => handleAddTopics()} 
+      type="button" 
+      className='bg-blue-500 rounded-md px-8 py-2 text-white mt-4'>Add New Topic</button>
+
     </div>
   );
 };
